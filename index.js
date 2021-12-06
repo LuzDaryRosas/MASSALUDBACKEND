63d882b2a09655ea7d5833541031241302563c67
Miguel Noriega <paymigue@gmail.com>
2021-12-06 17:22:14 -0500

    Fix Backend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let app = express();
 let bodyParser = require('body-parser')
 
 let cors = require('cors');
+/*
 app.use(cors({
     'allowedHeaders': ['sessionId', 'Content-Type'],
     'exposedHeaders': ['sessionId'],
@@ -11,31 +12,21 @@ app.use(cors({
     'preflightContinue': false,
     'optionsSuccessStatus': 200
 }));
+*/
 //middlewares
   app.use(cors());
   app.use(express.static('public'));
 
 app.use(express.json());
 app.use(express.urlencoded({ exteded: true }));     
-
+/*
 let router = express.Router();
 app.use(router);
-
+*/
 // aqui empece 
 
-var requestTime = function (req, res, next) {
-    req.requestTime = Date.now();
-    next();
-  };
-  
-  app.use(requestTime);
-  
-  app.get('/', function (req, res) {
-    var responseText = 'Hello World!';
-    responseText += 'Requested at: ' + req.requestTime + '';
-    res.send(responseText);
-  });
-
+  // settings
+app.set('port', process.env.PORT); //Setear puerto, ej: localhost:4000
 
 
 //
@@ -70,6 +61,6 @@ app.use('/api', tableroColRoute)
 // RUTAS
 */
 
-app.listen(3000, function () {
+app.listen(app.get('port'), function () {
     console.log("Node server running on http://localhost:3000");
 });
